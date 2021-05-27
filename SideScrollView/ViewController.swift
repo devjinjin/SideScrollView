95cc577cd429c824d042b98254f4882f67fa46e5
devjinjin <devjinjin@gmail.com>
2021-05-27 11:55:52 +0900

    Initial Commit

---
+++ SideScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideScrollView
+//
+//  Created by jylee-mac on 2021/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
